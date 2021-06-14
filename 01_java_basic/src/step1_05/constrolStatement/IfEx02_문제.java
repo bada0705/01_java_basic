7acebeed3037dd8b87bc470ffea55eb99bff3c32
12_web_cdy <12_web_cdy@DESKTOP-HB0IECH>
2021-06-14 13:38:09 +0900

    -

--- 01_java_basic/src/step1_05/constrolStatement/IfEx02_문제.java
+++ 01_java_basic/src/step1_05/constrolStatement/IfEx02_문제.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 /*
  * # Up & Down 게임[1단계]
  * 
- * 1. com은 8이다.
- * 2. me는 com의 숫자를 맞추는 게임이다.
+ * 1. com은  8이다.
+ * 2. me는 com의 숫자 를 맞추는 게임이다.
  * 3. 다음과 같은 메세지를 출력한다.
  * 1) me < com	: Up!
  * 2) me == com : Bingo!
